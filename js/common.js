6d8bec26003d2120109d68cbfee31e382ae728df
Валерий <kaktuz34@gmail.com>
2015-04-08 18:01:20 +0300

    init com

--- js/common.js
+++ js/common.js
@@ -1,19 +1,73 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	// slider
+	$('.js-slider').slick({
+		dots: false,
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		speed: 300,
+		cssEase: 'linear'
+	});
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+	// click
+	$(document).click(function() {
+		$(".js-select").removeClass("is-active");
+		$(".js-select-list").slideUp(100);
+	});
+	
 
-	console.log($('body').html());
+	// popup
+	$('.js-open-popup').on('click', function(){
+		$('.js-popup').show();
+		$('body').addClass('is-fixed');
+		return false;
+	});
+	$('.js-popup-overley, .js-popup-close').on('click', function(){
+		$('.js-popup').hide();
+		$('body').removeClass('is-fixed');
+	});
+
+	// select list
+	$("body").on("click", ".js-select",function(event) {
+		event.stopPropagation();
+	});
+
+	$("body").on("click",".js-select-text",function() {
+		var select = $(this).parents(".js-select");
+		if (select.hasClass("is-active")) {
+			$(".js-select").removeClass("is-active");
+			$(".js-select-list").slideUp(100);
+		}
+		else {
+			$(".js-select").removeClass("is-active");
+			$(".js-select-list").slideUp(100);
+			select.toggleClass("is-active").find(".js-select-list").slideToggle(100);
+		}
+		
+	});
+
+	$("body").on("click",".js-select-list li",function() {
+		var val = $(this).attr("data-val");
+		var text = $(this).text();
+		var select = $(this).parents(".js-select");
+		var selectList = $(this).parents(".js-select-list");
+		select.find(".js-select-text").text(text);
+		select.find("option").removeAttr("selected");
+		select.find('option[value="'+val+'"]').attr("selected", "selected");
+		selectList.find("li").removeClass("is-active");
+		$(this).addClass("is-active");
+		select.removeClass("is-active");
+		selectList.slideUp(100);
+		return false;
+	});
+	
+	// calendar
+	$( "#datepicker" ).datepicker({
+		dateFormat: 'd.mm.yy',
+		showOn: "both",
+		buttonImageOnly: false,
+		buttonImage: "img/calendar-ico.png"
+	});
+
 });
